c644f15e5c834011d3c1baf693c0ab3518ac9400
alexchambers <alexchambers91@gmail.com>
2014-07-29 16:07:45 -0700

    Uploading work from laptop to work computer (continuing to work)

--- app/src/main/java/com/alex/ben/greatestappevermade/MainActivity.java
+++ app/src/main/java/com/alex/ben/greatestappevermade/MainActivity.java
@@ -12,10 +12,10 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
-//for some reason could not resolve jjoe64 in Android studio, worked in Eclipse
+// GraphView class provided by jjoe64
+// Source: https://github.com/jjoe64/GraphView
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.GraphView.GraphViewData;
 import com.jjoe64.graphview.GraphViewSeries;
@@ -25,7 +25,9 @@ import java.util.Random;
 
 public class MainActivity extends ActionBarActivity {
 
+    // Database
     SQLiteDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +37,8 @@ public class MainActivity extends ActionBarActivity {
                     .commit();
         }
 
-        final GraphViewSeries exampleSeries = new GraphViewSeries( new GraphViewData[] { new GraphViewData(1, 0)}); //Initializes a graph with no data
+        // Create the graph
+        final GraphViewSeries dataSeries = new GraphViewSeries( new GraphViewData[] { new GraphViewData(1, 0)}); //Initializes a graph with no data
         final GraphView graphView = new LineGraphView(this, "Assignment 1");
         graphView.setManualYAxisBounds(1, 0);  //Sets Y-axis bounds, ensures that blank graph doesn't look odd
         graphView.getGraphViewStyle().setGridColor(Color.LTGRAY);
@@ -43,26 +46,22 @@ public class MainActivity extends ActionBarActivity {
         graphView.getGraphViewStyle().setVerticalLabelsColor(Color.BLUE);
         graphView.getGraphViewStyle().setNumHorizontalLabels(2);
         graphView.getGraphViewStyle().setNumVerticalLabels(5);
-        graphView.addSeries(exampleSeries);
+        graphView.addSeries(dataSeries);
 
-        //finds the display screen height
+        // Finds the display screen height
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
         int height = metrics.heightPixels;
 
-        //Makes the graph only take up 70% of the screen
+        // Makes the graph only take up 70% of the screen
         LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) graphView.getLayoutParams();
         params.height = (int) (.7 * height);
         graphView.setLayoutParams(params);
-        //Makes the graph only take up 70% of the screen
 
         final LinearLayout layout = (LinearLayout) findViewById(R.id.layout);
         layout.addView(graphView);
 
-
-
-
-        // Button that generates graph
+        // Button that generates the graph
         Button generateGraphButton = (Button) findViewById(R.id.startGraph);
         generateGraphButton.setOnClickListener(new OnClickListener() {
 
@@ -70,28 +69,26 @@ public class MainActivity extends ActionBarActivity {
             public void onClick(View v) {
                 Random rand = new Random();
                 //on each click, generates 4 random points of data
-                exampleSeries.resetData(new GraphViewData[] {
-                                new GraphViewData (1, rand.nextDouble()), new GraphViewData (2, rand.nextDouble()), new GraphViewData (3, rand.nextDouble()), new GraphViewData (4, rand.nextDouble())
+                dataSeries.resetData(new GraphViewData[]{
+                                new GraphViewData(1, rand.nextDouble()), new GraphViewData(2, rand.nextDouble()), new GraphViewData(3, rand.nextDouble()), new GraphViewData(4, rand.nextDouble())
                         }
                 );
                 graphView.getGraphViewStyle().setNumHorizontalLabels(4);
-                graphView.addSeries(exampleSeries);
+                graphView.addSeries(dataSeries);
             }
         });
 
+        // Button that clears the graph
         Button clearGraph = (Button) findViewById(R.id.clearGraph);
         clearGraph.setOnClickListener (new OnClickListener (){
             public void onClick (View v){
+
                 //resets to blank graph
-                exampleSeries.resetData(new GraphViewData[] {
-                                new GraphViewData (1, 0)
-                        }
-                );
+                dataSeries.resetData(new GraphViewData[]{ new GraphViewData(1, 0)} );
                 graphView.getGraphViewStyle().setNumHorizontalLabels(2);
-                graphView.addSeries(exampleSeries);
+                graphView.addSeries(dataSeries);
             }
         });
-
 
         final EditText patientname = (EditText) findViewById (R.id.name);
         final EditText patientage = (EditText) findViewById (R.id.age);
@@ -104,12 +101,14 @@ public class MainActivity extends ActionBarActivity {
             public void onClick(View arg0) {
                 int selected = genders.getCheckedRadioButtonId();
                 String NAME = getTableName(patientname, patientid, patientage, selected);
+
                 try{
                     db = openOrCreateDatabase (NAME, Context.MODE_PRIVATE, null);
                 } catch (SQLiteException e){
 
                 }
                 db.beginTransaction();
+
                 try{
                     db.execSQL("create table " + NAME + " ( recID integer PRIMARY KEY autoincrement, time text, x text, y text, z text );");
                     db.setTransactionSuccessful();
@@ -119,9 +118,7 @@ public class MainActivity extends ActionBarActivity {
                     db.endTransaction();
                 }
             }
-
         });
-
     }
     public String getTableName(EditText name, EditText id, EditText age, int selected){
         String sex;
@@ -130,8 +127,7 @@ public class MainActivity extends ActionBarActivity {
         } else {
             sex = "Male";
         }
-        String tblname = name.getText().toString() + "_" + id.getText().toString() + "_" + age.getText().toString() + "_" + sex;
-        return tblname;
+        // Return the table name
+        return (name.getText().toString() + "_" + id.getText().toString() + "_" + age.getText().toString() + "_" + sex);
     }
-
 }
